4a717df4c21a6e7d6efd0ac4ac4f172f2dc9109d
Vangelis Mitsianis <ev.mitsianis@gmail.com>
2025-05-25 09:14:55 +0300

    Add: Theme toggle

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "@/assets/styles/globals.css";
+import '@/assets/styles/globals.css';
 import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from '@/lib/constants';
+import type { Metadata } from 'next';
+import { ThemeProvider } from 'next-themes';
+import { Inter } from 'next/font/google';
 
 const inter = Inter({
-  subsets: ["latin"],
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
@@ -22,12 +23,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-    <body
-      className={`${inter.className} antialiased`}
-    >
-    {children}
-    </body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className} antialiased`}>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,8 +1,9 @@
-import Link from 'next/link';
-import Image from 'next/image';
-import { APP_NAME } from '@/lib/constants';
 import { Button } from '@/components/ui/button';
+import { APP_NAME } from '@/lib/constants';
 import { ShoppingCart, UserIcon } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
+import ModeToggle from './mode-toggle';
 
 const Header = () => {
   return (
@@ -15,12 +16,13 @@ const Header = () => {
           </Link>
         </div>
         <div className="space-x-2">
+          <ModeToggle />
           <Button asChild variant="ghost">
             <Link href="/cart">
               <ShoppingCart /> Cart
             </Link>
           </Button>
-          <Button asChild variant="ghost">
+          <Button asChild>
             <Link href="/sign-in">
               <UserIcon /> Sign in
             </Link>
